e0c51e06d87540014343557303a2525d0f160273
Christophe <christophe.misc+git@protonmail.ch>
2020-12-09 22:23:13 +0100

    Add error message/error summary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import discord
+from discord.ext import commands
 
 from discord.ext.commands import Bot
 
@@ -27,11 +28,33 @@ def parse_config_from_env() -> dict:
     }
 
 
+async def send_cmd_help(ctx):
+    cmd = ctx.command
+    em = discord.Embed(title=f'Usage: {ctx.prefix + cmd.name + " " + cmd.signature}', color=discord.Color.green())
+    em.description = cmd.help
+    return em
+
+
+async def on_command_error(ctx, error):
+    send_help = (commands.MissingRequiredArgument, commands.BadArgument, commands.TooManyArguments,
+                 commands.UserInputError)
+
+    if isinstance(error, commands.CommandNotFound):  # fails silently
+        pass
+
+    elif isinstance(error, send_help):
+        _help = await send_cmd_help(ctx)
+        await ctx.send(embed=_help)
+
+
 def main():
     load_dotenv()
     config: dict = parse_config_from_env()
 
     client = Bot(config["command_prefix"])
+
+    client.add_listener(on_command_error, "on_command_error")
+
     client._custom_config = config
     client.load_extension("chaos")
     client.run(config["token"])
