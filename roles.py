f2820d515f1d647cd61fe4c710a04b6aa5d0b08c
Christophe <christophe.misc+git@protonmail.ch>
2021-07-05 03:54:31 +0200

    [roles.py] Add some docstrings, formatting

--- roles.py
+++ roles.py
@@ -26,13 +26,32 @@ class Roles(Cog):
     @command()
     @is_mod()
     async def post_role_msg(self, ctx, reference: str):
+        """
+        Post or edit a message displaying buttons that can be clicked to get or un-get a specific roles.
+        Args:
+            ctx: (internal)
+            reference: identifier of role category to use for the specific message
+        """
+
         def role_to_button(role: dict) -> Button:
+            """
+            Create button component from role specification.
+            Args:
+                role (dict): { "title": title, "id": message id }
+            """
             if "emoji" in role:
                 return Button(style=ButtonStyle.grey, label=role["title"], id=str(role["id"]), emoji=role["emoji"])
             return Button(style=ButtonStyle.grey, label=role["title"], id=str(role["id"]))
 
         def split_if_necessary(l: list) -> list[list]:
-            return [l[i:i+5] for i in range(0, len(l), 5)]
+            """
+            If the list has more than 5 elements, split it into multiple lists of up to 5 elements.
+            Args:
+                l (list): List of button components
+            Returns:
+                list of lists of button components, each up to 5 elements
+            """
+            return [l[i:i + 5] for i in range(0, len(l), 5)]
 
         if reference not in self.roles:
             await ctx.send("Unknown reference given")
@@ -60,6 +79,9 @@ class Roles(Cog):
     @command()
     @is_mod()
     async def reload_role_categories(self, ctx):
+        """
+        Reload the role categories from roles.json.
+        """
         with open("roles.json") as file:
             self.roles = json.loads(file.read())
 
@@ -76,7 +98,8 @@ class Roles(Cog):
         role = self.guild.get_role(int(res.component.id))
         if role is None:
             await res.respond(
-                type=InteractionType.ChannelMessageWithSource, content=f"Error: could not find role {res.component.label}"
+                type=InteractionType.ChannelMessageWithSource,
+                content=f"Error: could not find role {res.component.label}"
             )
             print(f"Error: could not find role {res.component.label}")
             return
